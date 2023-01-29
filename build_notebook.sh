c9ac486d74619a658654e29d995528d049471f40
Laurence P. Abanes <laurence@Laurences-MacBook-Air.local>
2023-01-29 10:38:50 +0900

    Added setup config

--- build_notebook.sh
+++ build_notebook.sh
@@ -3,6 +3,6 @@
 pip3 install --upgrade pip
 pip3 install --user pipenv
 
-python3 -m pipenv install -r requirements.txt
+python3 -m pipenv install .
 python3 -m pipenv run pre-commit install
 python3 -m pipenv run python python-scripts/build-notebook.py
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+from __version__ import version
+
+
+if __name__ == "__main__":
+    setup()
